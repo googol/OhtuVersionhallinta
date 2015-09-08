493159779a87d881a38a4a7e55e1a8d71c0565bd
Miika Hänninen <miika.hanninen@gmail.com>
2015-09-09 00:44:44 +0300

    Implementoi init-toiminnon

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
@@ -9,10 +10,29 @@ fn main() {
                       .map(|val| &val[..]);
 
     match command {
+        Some("init") => init_repository(),
         _ => print_help(),
+    }
+}
+
+fn init_repository() {
+    let mut directory = env::current_dir().unwrap();
+    directory.push(".ohtuv");
+
+    if let Ok(_) = fs::metadata(&directory) {
+        println!("The repository has already been initialized.");
+        return;
+    }
+
+    println!("Initializing repository in {:?}", &directory);
+    match fs::create_dir(&directory) {
+        Ok(_) => println!("Finished"),
+        Err(_) => println!("Failed. Check your directory permissions."),
     }
 }
 
 fn print_help() {
     println!("usage: ohtuv <command>");
+    println!("Commands:");
+    println!("    init - Initializes a repository in the current directory if none exists.");
 }
