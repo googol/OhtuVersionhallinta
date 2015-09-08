5de9a952ab83abe7ce596827cc711b26bb8c2b75
Miika Hänninen <miika.hanninen@gmail.com>
2015-09-09 00:11:23 +0300

    Lisää peruspohjan ohjelmalle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<_>>();
+
+    // Kokeillaan ottaa ensimäinen parametri, ja mapataan se
+    // tyyppiin &str slicing-syntaksilla
+    let command = args.get(1)
+                      .map(|val| &val[..]);
+
+    match command {
+        _ => print_help(),
+    }
+}
+
+fn print_help() {
+    println!("usage: ohtuv <command>");
 }
