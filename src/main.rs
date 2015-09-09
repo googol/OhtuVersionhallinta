27f767da4c920886cce34050698e9cf0c92e268b
Miika Hänninen <miika.hanninen@gmail.com>
2015-09-09 11:14:42 +0300

    Lisää metodin .ohtuv-repositorion etsimiseen

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::path::PathBuf;
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
@@ -11,6 +12,7 @@ fn main() {
 
     match command {
         Some("init") => init_repository(),
+        Some("find") => find_repository_command(),
         _ => print_help(),
     }
 }
@@ -31,8 +33,39 @@ fn init_repository() {
     }
 }
 
+fn find_repository_command() {
+    match find_repository() {
+        Some(directory) => println!("Repository is at {:?}.", directory),
+        None => println!("Repository not found."),
+    }
+}
+fn find_repository() -> Option<Box<PathBuf>> {
+    let mut directory_box = Box::new(env::current_dir().unwrap());
+    let success = walk_parent_directories(&mut *directory_box);
+    if success {
+        Some(directory_box)
+    } else {
+        None
+    }
+}
+
+fn walk_parent_directories(directory: &mut PathBuf) -> bool {
+    while directory.file_name() != None {
+        directory.push(".ohtuv");
+        if let Ok(_) = fs::metadata(&directory) {
+            return true;
+        }
+
+        // Popataan ensin .ohtuv pois, sitten uudestaan että päästään ylemmälle tasolle
+        directory.pop();
+        directory.pop();
+    }
+    false
+}
+
 fn print_help() {
     println!("usage: ohtuv <command>");
     println!("Commands:");
     println!("    init - Initializes a repository in the current directory if none exists.");
+    println!("    find - Finds and prints the nearest repository up the directory tree.");
 }
